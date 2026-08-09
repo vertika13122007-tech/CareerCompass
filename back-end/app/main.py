4fb6c236aa676c2657bc645add73906226e0373f
vertika13122007-tech <vertika13122007@gmail.com>
2026-08-09 10:21:28 +0530

    Implemented the life span structure for the AI models

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import User
@@ -10,7 +11,21 @@ from app.routers.profile import router as profile_router
 from app.routers.resume import router as resume_router
 from app.routers.job_preference import router as job_pref_router
 
-app = FastAPI();
+ml_models = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML models here
+    print("🚀 Loading ML models into memory...")
+    ml_models["dummy_model"] = "Model loaded successfully!" # Placeholder for real models
+    yield
+    # Clean up resources on shutdown
+    print("🛑 Cleaning up ML models...")
+    ml_models.clear()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(resume_router)
@@ -18,8 +33,10 @@ app.include_router(job_pref_router)
 
 Base.metadata.create_all(bind=engine)
 
+
 @app.get("/")
 def root():
-    return{
-        "message":"CareerCompass AI API Running"
+    return {
+        "message": "CareerCompass AI API Running"
     }
+
